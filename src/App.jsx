5d5e042a226cc373c1f08e5a43220000336925f9
TM <1476mahalakshmi@gmail.com>
2025-12-23 14:15:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -352,7 +352,7 @@ const App = () => {
       `}</style>
 
       <header className="header">
-        <h1 className="brand">FutureFund</h1>
+        <h1 className="brand">Financal Growth Simulator</h1>
         <div className="controls">
           <div ref={currencyRef} style={{ position: 'relative' }}>
             <button className="icon-btn" onClick={() => setIsCurrencyMenuOpen(!isCurrencyMenuOpen)}>
